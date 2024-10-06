ba0e21e6d986061a3f0e169691a4c2f040c49f00
espennoreng <espennoreng@gmail.com>
2024-10-06 14:24:43 +0200

    add firebase

---
+++ firebase/functions/src/index.ts
@@ -0,0 +1,12 @@
+import { onRequest } from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+const helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello from Firebase!");
+});
+
+export default helloWorld;
