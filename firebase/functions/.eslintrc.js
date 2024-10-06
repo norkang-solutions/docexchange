27e40428e9d9327490045b19de122af78d5b53d5
Espen Noreng <81035378+espennoreng@users.noreply.github.com>
2024-10-06 15:34:16 +0200

    feat: add firebase (#2)

---
+++ firebase/functions/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    root: true,
+    env: {
+        es6: true,
+        node: true,
+    },
+    extends: [
+        "plugin:import/errors",
+        "plugin:import/warnings",
+        "plugin:import/typescript",
+        "airbnb",
+        "prettier",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: ["tsconfig.json", "tsconfig.dev.json"],
+        sourceType: "module",
+    },
+    ignorePatterns: [
+        "/lib/**/*", // Ignore built files.
+        "/generated/**/*", // Ignore generated files.
+    ],
+    plugins: ["@typescript-eslint", "import"],
+    rules: {
+        quotes: ["error", "double"],
+        "import/no-unresolved": 0,
+        "import/extensions": "off",
+        indent: ["error", 2],
+        "no-console": "error",
+        "import/prefer-default-export": "off",
+    },
+};
