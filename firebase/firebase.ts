9b0a1c5f07c86ebe9dfb05d8b6aa77a4565a133e
espennoreng <espennoreng@gmail.com>
2024-10-07 19:10:00 +0200

    add emulator initalization in dev

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -1,13 +1,19 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { connectAuthEmulator, getAuth } from "firebase/auth";
 import firebaseConfig from "./config";
-import { getStorage } from "firebase/storage";
-import { getFirestore } from "firebase/firestore";
+import { connectStorageEmulator, getStorage } from "firebase/storage";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
+
+if (process.env.NODE_ENV === "development") {
+    connectAuthEmulator(auth, "http://localhost:9099");
+    connectFirestoreEmulator(db, "localhost", 8080);
+    connectStorageEmulator(storage, "localhost", 9199);
+}
 
 export { auth, db, storage };
