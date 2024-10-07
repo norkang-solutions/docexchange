ce3d77c6550d053dc01621d06eb252f0f24afc7a
espennoreng <espennoreng@gmail.com>
2024-10-07 22:02:55 +0200

    add error field to input component

--- app/_components/input.tsx
+++ app/_components/input.tsx
@@ -1,11 +1,13 @@
 import { InputHTMLAttributes } from "react";
 import { twJoin } from "tailwind-merge";
+import ErrorP from "./error-p";
 
 type InputProps = InputHTMLAttributes<HTMLInputElement> & {
     label: string;
+    error?: string;
 };
 
-export default function Input({ label, ...props }: InputProps) {
+export default function Input({ label, error, ...props }: InputProps) {
     return (
         <div className="flex flex-col gap-1">
             <label
@@ -16,13 +18,14 @@ export default function Input({ label, ...props }: InputProps) {
             </label>
             <input
                 className={twJoin(
-                    "px-3 py-2 text-base border border-slate-300 rounded-lg",
+                    "px-3 py-2 text-base bg-white border border-slate-300 rounded-lg",
                     "text-slate-900 placeholder:text-slate-400",
                     "hover:border-slate-400",
                     "focus:outline-none focus:ring-1 focus:ring-slate-900 focus:border-slate-900"
                 )}
                 {...props}
             />
+            {error && <ErrorP>{error}</ErrorP>}
         </div>
     );
 }
