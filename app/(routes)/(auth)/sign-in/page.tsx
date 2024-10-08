1f4377482f0f57653bdf02836ef94cb754342c84
espennoreng <espennoreng@gmail.com>
2024-10-08 19:26:25 +0200

    add metadata to pages

--- app/(routes)/(auth)/sign-in/page.tsx
+++ app/(routes)/(auth)/sign-in/page.tsx
@@ -1,4 +1,9 @@
+import { Metadata } from "next";
 import SignInForm from "./form";
+
+export const metadata: Metadata = {
+    title: "Sign In",
+};
 
 export default function SignInPage() {
     return (
--- app/(routes)/(auth)/sign-up/page.tsx
+++ app/(routes)/(auth)/sign-up/page.tsx
@@ -1,4 +1,9 @@
+import { Metadata } from "next";
 import SignUpForm from "./form";
+
+export const metadata: Metadata = {
+    title: "Sign Up",
+};
 
 export default function SignUp() {
     return (
