4f53fe755c163ea29f05f3f3299c1c83f3df715c
espennoreng <espennoreng@gmail.com>
2024-10-08 18:54:40 +0200

    add sign-in page

---
+++ app/(routes)/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import SignInForm from "./form";
+
+export default function SignInPage() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <SignInForm />
+        </div>
+    );
+}
