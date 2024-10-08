20bbf002f71ff4fea13068fe1b309a5741c09876
espennoreng <espennoreng@gmail.com>
2024-10-08 20:10:03 +0200

    remove email display in navbar

--- app/_components/nav-bar.tsx
+++ app/_components/nav-bar.tsx
@@ -3,11 +3,6 @@
 import { AuthProvider, useAuth } from "../_contexts/auth-context";
 import Button from "./button";
 
-function DisplayUser() {
-    const { user } = useAuth();
-    return <div>{user?.email}</div>;
-}
-
 function SignOutButton() {
     const { signOut } = useAuth();
     return <Button onClick={signOut}>Sign Out</Button>;
@@ -19,7 +14,6 @@ export default function NavBar() {
             <div className="flex justify-between items-center p-4">
                 <h1 className="text-2xl font-bold">NavBar</h1>
                 <div className="flex gap-2 items-center">
-                    <DisplayUser />
                     <SignOutButton />
                 </div>
             </div>
