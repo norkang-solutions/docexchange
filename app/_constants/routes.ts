a7e9c10d87f06a0fcf639bda91a69a87a0053f58
espennoreng <espennoreng@gmail.com>
2024-10-13 15:20:39 +0200

    add constants

---
+++ app/_constants/routes.ts
@@ -0,0 +1,7 @@
+export enum ROUTES {
+    Home = "/",
+    SIGN_UP = "/sign-up",
+    SIGN_IN = "/sign-in",
+    PRIVACY_POLICY = "/privacy-policy",
+    TERMS_AND_CONDITIONS = "/terms-and-conditions",
+}
