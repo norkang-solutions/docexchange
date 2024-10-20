f8e91d79b07b0cc9fbf04c4c5e25638333bbbc81
espennoreng <espennoreng@gmail.com>
2024-10-20 11:40:30 +0200

    fix spacing

--- app/_components/checkbox.tsx
+++ app/_components/checkbox.tsx
@@ -15,7 +15,7 @@ export default function Checkbox({ error, children, ...props }: CheckboxProps) {
                 <input
                     className={twJoin(
                         "peer relative appearance-none shrink-0 w-5 h-5 mt-1 border border-slate-300 rounded-md",
-                        "checked:bg-emerald-500  checked:border-emerald-500",
+                        "checked:bg-emerald-500 checked:border-emerald-500",
                         "focus:outline-none focus:ring-1 focus:ring-slate-900 focus:border-slate-900"
                     )}
                     type="checkbox"
