c180b584c2f47360933af151cf8cbf890e28566a
espennoreng <espennoreng@gmail.com>
2024-10-22 22:45:00 +0200

    fix spacing

--- app/(routes)/[lang]/dashboard/profile/page.tsx
+++ app/(routes)/[lang]/dashboard/profile/page.tsx
@@ -11,14 +11,10 @@ export default function Profile({ params: { lang } }: LangParams) {
     return (
         <div className="px-4 flex flex-col gap-4">
             <h1 className="text-2xl font-bold">{dict.your_profile}</h1>
-
             <div className="flex flex-col gap-4 max-w-sm w-full">
                 <UpdateUserForm dict={dict} />
-
                 <Divider text={dict.or} />
-
                 <SignOutButton dict={dict} />
-
                 <Button variant="secondary">{dict.change_password}</Button>
                 <Button variant="secondary">{dict.delete_account}</Button>
             </div>
